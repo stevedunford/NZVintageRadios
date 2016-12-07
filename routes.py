3eefe1c92103253d208f612916c121e1e3883674
Steve <steve@dunford.org.nz>
2016-12-07 21:03:29 +1300

    basic file uploading added

--- routes.py
+++ routes.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask, flash, session, request, render_template, url_for
 
 app = Flask(__name__)
 app.secret_key = "~1\xe4\xa77b\x04\x0c\xcc1\x89\xb9\xce\x1c\xa1H\xc7\x82\xe2\xc3\x97\xe9\x13z"
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 
 @app.route("/")
 def index():
@@ -16,6 +19,26 @@ def echo():
 @app.route("/radio1")
 def radio1():
 	return render_template("index.html", rad=True)
+
+@app.route("/uploads", methods=['POST'])
+def uploads():
+	target=os.path.join(APP_ROOT, 'images/')
+	print("images dir: " + target)
+
+	# check to see if images directory exists, make if not
+	if not os.path.isdir(target):
+		os.mkdir(target)
+
+	for file in request.files.getlist("file"):
+		print ("filename: " + str(file))
+		filename = file.filename
+		destination = "/".join([target, filename])
+		print("file being stored in: " + destination)
+		file.save(destination)
+
+	return render_template("index.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
