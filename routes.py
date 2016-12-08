3716befeac9b4f54625d9f0b8be282ce08079f85
stevedunford <steve@dunford.org.nz>
2016-12-08 22:35:43 +1300

    Starting to sort database

--- routes.py
+++ routes.py
@@ -1,9 +1,15 @@
 import os
 from flask import Flask, flash, session, request, render_template, url_for
+from flaskext.mysql import MySQL
 
+mysql = MySQL()
 app = Flask(__name__)
 app.secret_key = "~1\xe4\xa77b\x04\x0c\xcc1\x89\xb9\xce\x1c\xa1H\xc7\x82\xe2\xc3\x97\xe9\x13z"
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+app.config['MYSQL_DATABASE_USER'] = 'nzvr_db'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'nzvr_db'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
 
 @app.route("/")
