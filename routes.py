52f1722658ea130cb43dee6956ead808578afefc
Steve <steve@dunford.org.nz>
2016-12-31 00:41:58 +1300

    Major database changes
    Using labels rather than arbitrary indexing to get data from database -
    means rewriting much of the code, but it needs doing for simplicity as
    things get more complex.  Part way there, brands should be working,
    models should be working.

--- routes.py
+++ routes.py
@@ -14,6 +14,7 @@ Licence: Beerware.  I need a beer, you need a website - perfect
 import os # for file upload path determination
 from flask import Flask, flash, session, request, render_template_string, render_template, redirect, url_for, g, abort
 from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
 from flask_wtf import Form
 from wtforms import StringField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired
@@ -28,7 +29,7 @@ app = Flask(__name__)
 app.secret_key = '}g\x83^jSvlazDyhxHCV^YUXw\\crxOddpw~\x83Q\x80eP|ZZqOSBcO\x80IGvlphvZkqCcv~\x7foJJHgYou|]rxvSDZN\x82OIo{Qis55N}Mo\x7fJK\x80G|Hdt'
 
 # MySQL config
-mysql = MySQL()
+mysql = MySQL(cursorclass=DictCursor)
 app.config['MYSQL_DATABASE_USER'] = 'nzvr_db'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
 app.config['MYSQL_DATABASE_DB'] = 'nzvr_db'
@@ -58,47 +59,99 @@ def index():
     return render_template("index.html", title="Welcome")
 
 
-@app.route("/radio/<brand>/<id>")
-@app.route("/radio/<brand>/<id>/<variant>")
-def radio(brand, id, variant=None):
-    conn = mysql.connect()
-    cursor = conn.cursor()
-
+@app.route("/model/<brand>/<id>")
+@app.route("/model/<brand>/<id>/<variant>")
+def model(brand, id, variant=None):
     # find the db's id for the brand name
-    cursor.execute("SELECT id, manufacturer_id FROM brand WHERE alias='{0}'".format(brand))
-    _brand, _manufacturer = cursor.fetchone()
+    result = query_db("SELECT id, manufacturer_id FROM brand WHERE alias='{0}'".format(brand), single=True)
+    _brand = result['id']
+    _manufacturer = result['manufacturer_id']
     
     # find the db's id for the radio model number
     if variant:
-        cursor.execute("SELECT id FROM radio WHERE number='{0}' AND name='{1}'".format(id, variant))
+        result = query_db("SELECT id FROM model WHERE number='{0}' AND name='{1}'".format(id, variant), single=True)
     else:
-        cursor.execute("SELECT id FROM radio WHERE number='{0}'".format(id))
-    radio_id = cursor.fetchone()[0]
-    
+        result = query_db("SELECT id FROM model WHERE number='{0}'".format(id), single=True)
+    if not result: # check to make sure a model was found
+        abort(404)
+    model_id = result['id']
+
     # get the variant if specified, or all matching radios
     if variant:
-        cursor.execute("SELECT * FROM radio WHERE brand_id='{0}' AND number='{1}' AND name='{2}'".format(_brand, id, variant))
+        models = query_db("SELECT * FROM model WHERE brand_id='{0}' AND number='{1}' AND name='{2}'".format(_brand, id, variant))
     else:
-        cursor.execute("SELECT * FROM radio WHERE brand_id='{0}' AND number='{1}'".format(_brand, id))
-    models = cursor.fetchall()
+        models = query_db("SELECT * FROM model WHERE brand_id='{0}' AND number='{1}'".format(_brand, id))
     
     # get the manufacturer and alias (for link)
-    cursor.execute("SELECT name, alias FROM manufacturer WHERE id='{0}'".format(_manufacturer))
-    manufacturer, manufacturer_alias = cursor.fetchone()
-
-    # get the images related to the radio (or primary image for each variant)
-    cursor.execute("SELECT name, path, thumb, rank FROM images WHERE type=1 AND type_id={0} ORDER BY rank ASC".format(radio_id))
-    images = cursor.fetchall()
-    print(images)
-
+    result = query_db("SELECT name, alias FROM manufacturer WHERE id='{0}'".format(_manufacturer), single=True)
+    manufacturer = result['name']
+    manufacturer_alias = result['alias']
+    
+    # get the images related to the radio (or primary image for each variant). type=1 means model images
+    if len(models) > 1:
+        images = []
+        for mod in models:
+            images.append(query_db("SELECT thumb FROM images WHERE rank=1 AND type=1 AND type_id={0}".format(mod['id']))[0])
         
-    if models is None:
-        return "NONE!"
-    else:
-        return render_template("radio.html", models=models, title=brand+' '+id, brand=brand, manufacturer=manufacturer, manufacturer_alias=manufacturer_alias, id=id, variant=variant, images=images)
+    else: # get all the images for the single model
+        images = query_db("SELECT name, path, thumb, rank FROM images WHERE type=1 AND type_id={0} ORDER BY rank ASC".format(model_id))
+        
+    return render_template("model.html", models=models, title=brand+' '+id, brand=brand, manufacturer=manufacturer, manufacturer_alias=manufacturer_alias, id=id, variant=variant, images=images)
 
 
     
+@app.route("/brands", methods=['GET', 'POST'])
+def brands():
+    if request.method == "POST":
+        return redirect(url_for('brand', id=request.form.get('id')))
+    else:
+        brands = query_db("SELECT alias, name FROM brand ORDER BY name ASC")
+        print (brands)
+        if brands is None:
+            out = []
+        else:
+            out = [[str(item['alias']), str(item['name'])] for item in brands]
+        
+        return render_template("brands.html", brands=out, title='NZ Radio Brands')
+                
+
+'''
+brand(id) takes an id which is either numeric or a string.
+brands() passes it an int, taken from the brand table in the
+db which is converted to a string in the 'try' section in
+order to make a tidy url.  It then runs again with the string
+type id and the lookup is done.  There may be a tidier way to
+do this...
+'''
+@app.route("/brand/<id>")
+def brand(id=None):
+    try:
+        id = int(id) #id number - database id
+        _brand = query_db("SELECT alias FROM brand WHERE id={0}".format(id), single=True)
+        return redirect(request.url.replace('/brand/{0}'.format(id), '/brand/{0}'.format(_brand[0].lower().strip().replace(' ', '_'))))
+    except ValueError: # id is not an int, ie: redirect worked
+        # find the info for this brand
+        _brand = query_db("SELECT * FROM brand WHERE alias='{0}'".format(id), single=True)
+        
+        #find the brand id
+        brand_id = (query_db("SELECT id FROM brand WHERE alias='{0}'".format(id), single=True))['id']
+        
+        # find all models for this brand
+        _models = (query_db("SELECT DISTINCT start_year, number FROM model WHERE brand_id='{0}'".format(brand_id)))
+        #if _models is None:
+        #    _models = []
+        #else:
+        #    _models = [(item[0], str(item[1])) for item in _models]
+        _logo = url_for('static', filename='images/brands/{0}.jpg'.format(_brand['alias']))
+    except: # if all else fails
+        abort(404)        
+
+    if _brand is None:
+        return "NONE!"
+    else:
+        return render_template("brand.html", title=_brand['name'], brand=_brand, logo=_logo, models=_models)
+  
+
 @app.route('/new_distributor', methods=['GET', 'POST'])
 def new_distributor():
     form = Distributor()
@@ -170,10 +223,7 @@ def manufacturers():
     if request.method == "POST":
         return redirect(url_for('manufacturer', id=request.form.get('id')))
     else:
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        cursor.execute("SELECT id, name FROM manufacturer ORDER BY name ASC")
-        manufacturers = cursor.fetchall()
+        manufacturers = query_db("SELECT id, name FROM manufacturer ORDER BY name ASC")
         if manufacturers is None:
             out = []
         else:
@@ -183,23 +233,25 @@ def manufacturers():
                 
 @app.route("/manufacturer/<id>")
 def manufacturer(id=None):
-    conn = mysql.connect()
-    cursor = conn.cursor()
     try:
         id = int(id) #id number - database id
-        cursor.execute("SELECT alias FROM manufacturer WHERE id={0}".format(id))
-        _manufacturer=cursor.fetchone()
+        _manufacturer = query_db("SELECT alias FROM manufacturer WHERE id={0}".format(id), single=True)
         return redirect(request.url.replace('/manufacturer/{0}'.format(id), '/manufacturer/{0}'.format(_manufacturer[0].lower().strip().replace(' ', '_'))))
     except ValueError: # id is not an int, ie: redirect worked
         # find the manufacturers details
-        cursor.execute("SELECT * FROM manufacturer WHERE alias='{0}'".format(id))
-        _manufacturer=cursor.fetchone()
+        _manufacturer = query_db("SELECT * FROM manufacturer WHERE alias='{0}'".format(id), single=True)
         
+        _new_co = None
+        if _manufacturer['became']:
+            _new_co = query_db("SELECT name, alias FROM manufacturer WHERE id={0}".format(_manufacturer['became']), single=True)
+        print("==================")
+        print(_new_co)
+        print("==================")
+            
         # find all models currently held for this manufacturer
-        cursor.execute("SELECT * FROM brand WHERE manufacturer_id='{0}'".format(_manufacturer[0]))
-        _brands=cursor.fetchall()
+        _brands = query_db("SELECT * FROM brand WHERE manufacturer_id='{0}'".format(_manufacturer['id']))
         
-        _logo = url_for('static', filename='images/manufacturers/{0}.jpg'.format(_manufacturer[2]))
+        _logo = url_for('static', filename='images/manufacturers/{0}.jpg'.format(_manufacturer['alias']))
         if not os.path.isfile(APP_ROOT + _logo):
             _logo = None
     except: # if all else fails
@@ -208,77 +260,14 @@ def manufacturer(id=None):
     if _manufacturer is None:
         return "NONE!"
     else:
-        return render_template("manufacturer.html", manufacturer=_manufacturer, title=_manufacturer[1], brands=_brands, logo=_logo)
-  
+        return render_template("manufacturer.html", manufacturer=_manufacturer, title=_manufacturer['name'], new_co=_new_co, brands=_brands, logo=_logo)
+ 
 
-
-@app.route("/brands", methods=['GET', 'POST'])
-def brands():
-    if request.method == "POST":
-        return redirect(url_for('brand', id=request.form.get('id')))
-    else:
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        cursor.execute("SELECT id, name FROM brand ORDER BY name ASC")
-        brands = cursor.fetchall()
-        print (brands)
-        if brands is None:
-            out = []
-        else:
-            out = [[str(item[0]), str(item[1])] for item in brands]
-        
-        return render_template("brands.html", brands=out, title='NZ Radio Brands')
-                
-
-'''
-brand(id) takes an id which is either numeric or a string.
-brands() passes it an int, taken from the brand table in the
-db which is converted to a string in the 'try' section in
-order to make a tidy url.  It then runs again with the string
-type id and the lookup is done.  There may be a tidier way to
-do this...
-'''
-@app.route("/brand/<id>")
-def brand(id=None):
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    try:
-        id = int(id) #id number - database id
-        cursor.execute("SELECT alias FROM brand WHERE id={0}".format(id))
-        _brand=cursor.fetchone()
-        return redirect(request.url.replace('/brand/{0}'.format(id), '/brand/{0}'.format(_brand[0].lower().strip().replace(' ', '_'))))
-    except ValueError: # id is not an int, ie: redirect worked
-        # find the info for this brand
-        cursor.execute("SELECT * FROM brand WHERE alias='{0}'".format(id))
-        _brand=cursor.fetchone()
-        print(_brand)
-        print()
-        
-        #find the brand id
-        cursor.execute("SELECT id FROM brand WHERE alias='{0}'".format(id))
-        brand_id=cursor.fetchone()[0]
-        
-        # find all models for this brand
-        cursor.execute("SELECT DISTINCT number FROM radio WHERE brand_id='{0}'".format(brand_id))
-        _models=cursor.fetchall()
-        if _models is None:
-            _models = []
-        else:
-            _models = [str(item[0]) for item in _models]
-        print(_models)
-        print()
-        
-            
-        
-        _logo = url_for('static', filename='images/brands/{0}.jpg'.format(_brand[2]))
-    except: # if all else fails
-        abort(404)        
-
-    if _brand is None:
-        return "NONE!"
-    else:
-        return render_template("brand.html", title=_brand[1], brand=_brand, logo=_logo, models=_models)
-  
+@app.route("/tips")
+def tips():
+    return render_template("tips.html", title="Tips & Tricks")
+    
+    
 @app.route("/echo", methods=['POST'])
 def echo():
     flash(request.form['text'])
@@ -315,7 +304,19 @@ def uploads():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html', title="404"), 404
+
+
+def query_db(query, single=False):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    result = cursor.fetchone() if single else cursor.fetchall()
+    print("================================")
+    print(result)
+    print("================================")
+    return result
+    
 
 
 if __name__ == "__main__":
